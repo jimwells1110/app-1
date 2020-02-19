59f533937ead83faf154a7d70a99d12f311262b6
Jim Wells <jwells.detail@gmail.com>
2020-02-18 20:50:11 -0800

    initial commit

---
+++ app 1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  app 1
+//
+//  Created by James wells on 2/18/20.
+//  Copyright © 2020 James wells. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Wabbit!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
